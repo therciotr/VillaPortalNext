9d9f1fe9b8c097c72e0b45a85fc81d6090444b11
rodlferreira <rodriggo.lferreira@icloud.com>
2021-05-01 01:07:03 -0300

    testes da grid do Rooms com o texto descritivo

--- src/components/Rooms/index.tsx
+++ src/components/Rooms/index.tsx
@@ -1,10 +1,74 @@
-import { Image, Box } from "@chakra-ui/react"
+import { Image, Box, Container, Text, Badge } from "@chakra-ui/react"
 
 
-export default function Rooms() {
-    return(
-        <Box boxSize="sm">
-            <Image src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
+export default function AirbnbExample() {
+
+        
+const property = {
+    imageUrl: "https://bit.ly/2Z4KKcF",
+    imageAlt: "Rear view of modern home with pool",
+    beds: 3,
+    baths: 2,
+    title: "Modern home in city center in the heart of historic Los Angeles",
+    formattedPrice: "$1,900.00",
+    reviewCount: 34,
+    rating: 4,
+}
+
+return (
+    <Box bg="tomato" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" ml="130px" mt="90px" mb="90px" >
+    <Image src={property.imageUrl} alt={property.imageAlt} />
+    <Text mr="10px">teste</Text> /*testando o posicionamento do texto*/
+
+    <Box p="6">
+        <Box d="flex" alignItems="baseline">
+        <Badge borderRadius="full" px="2" colorScheme="teal">
+            New
+        </Badge>
+        <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+            
+        >
+            {property.beds} beds &bull; {property.baths} baths
         </Box>
-    );
+        </Box>
+
+        <Box
+        mt="1"
+        fontWeight="semibold"
+        as="h4"
+        lineHeight="tight"
+        isTruncated
+        >
+        {property.title}
+        </Box>
+
+        <Box>
+        {property.formattedPrice}
+        <Box as="span" color="gray.600" fontSize="sm">
+            / wk
+        </Box>
+        </Box>
+
+        <Box d="flex" mt="2" alignItems="center">
+        {/* {Array(5)
+            .fill("")
+            .map((_, i) => (
+            <StarIcon
+                key={i}
+                color={i < property.rating ? "teal.500" : "gray.300"}
+            />
+            ))} */}
+        <Box as="span" ml="2" color="gray.600" fontSize="sm">
+            {property.reviewCount} reviews
+        </Box>
+        </Box>
+    </Box>
+    </Box>
+);
 }
