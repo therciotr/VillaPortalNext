5147068edbed3b2a2be696aaf29d0a938993f9d6
rodlferreira <rodriggo.lferreira@icloud.com>
2021-05-05 13:44:46 -0300

    correcao do line.svg e inclusado do componente uttonSeeMore iniciando a criacao do mapa

---
+++ src/components/Rooms/ButtonSeeMore.tsx
@@ -0,0 +1,9 @@
+import { Button, Flex, ButtonGroup } from "@chakra-ui/react";
+
+export function ButtonSeeMore() {
+    return(
+        <Flex>
+            <Button bg="#FEC200" color="#FFFFFF">Ver mais...</Button>
+        </Flex>
+    );
+}
