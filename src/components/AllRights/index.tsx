2d1307c60d6f7ac02215bada72023aaf59802bb5
rodlferreira <rodriggo.lferreira@icloud.com>
2021-05-16 14:59:22 -0300

    correcao do footer e do AllRights

--- src/components/AllRights/index.tsx
+++ src/components/AllRights/index.tsx
@@ -3,8 +3,28 @@ import { Flex, Box, Grid, Text } from "@chakra-ui/react";
 
 export default function AllRights() {
     return(
-        <Flex bg="#292929">
-            <Text color="#FFFFFF" align="right"> © Todos os direitos reservados - 2021 </Text>
-        </Flex>
+        <Grid direction="column" align="center" bg="#292929">
+            
+            <Text 
+            color="#FFFFFF" 
+            align="right" 
+            margin="5px"
+            position="relative"
+            right="100px"
+            > 
+                © Todos os direitos reservados - 2021 
+            </Text>
+
+            <Text 
+            color="#FFFFFF" 
+            align="left" 
+            position="absolute"
+            margin="5px"
+            left="50px"
+            > 
+                Desenvolvido por TR Tecnologias 
+            </Text>
+            
+        </Grid>
     );
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -16,48 +16,105 @@ import { Button, ButtonGroup } from "@chakra-ui/react"
 
 export default function Footer() {
     return (
-      <Flex bg="#4E4B4B" width="100%" height="100%">
-        <Flex>
-          <Image src="/images/LOGOTIPO VILLA PORTAL- FOOTER.svg" alt="Logo" />
-        </Flex>
-        <Flex>
+      
+      // <Grid bg="#4E4B4B" width="100%" height="100%">
+      //   <Flex>
+      //     <Image src="/images/LOGOTIPO VILLA PORTAL- FOOTER.svg" alt="Logo" />
+      //   </Flex>
+      //   <Flex>
         
-        <Flex mt="150px">
-          <HStack>
+      //   <Flex mt="150px">
+      //     <HStack>
 
-          <Button bg="#F6E05E" height="38px" width="38px">
-            <Image src="/images/facebook.svg" alt="Logo" />
-          </Button>  
+      //     <Button bg="#F6E05E" height="38px" width="38px">
+      //       <Image src="/images/facebook.svg" alt="Logo" />
+      //     </Button>  
 
-          <Button bg="#F6E05E" height="38px" width="38px">
-            <Image src="/images/instagram.svg" alt="Logo" />
-          </Button>   
+      //     <Button bg="#F6E05E" height="38px" width="38px">
+      //       <Image src="/images/instagram.svg" alt="Logo" />
+      //     </Button>   
 
-          <Button bg="#F6E05E" height="38px" width="38px">
-            <Image src="/images/youtube.svg" alt="Logo" />
-          </Button> 
+      //     <Button bg="#F6E05E" height="38px" width="38px">
+      //       <Image src="/images/youtube.svg" alt="Logo" />
+      //     </Button> 
          
-          </HStack> 
-          </Flex>
+      //     </HStack> 
+      //     </Flex>
 
-          <Grid >
-          <Stack justifyContent="right" direction="row" spacing={4} align="center">
-            <Button colorScheme="#FFFFFF" color="#FFFFFF" variant="link">
-              Termo de uso
-            </Button>
+      //     {/* <Grid bg="pink"> */}
 
-            <Button colorScheme="#FFFFFF" color="#FFFFFF" variant="link">
-               Direitos autorais
-            </Button>
+      //       <ButtonGroup spacing="6" bg="pink">
+      //       <Button colorScheme="#FFFFFF" color="#FFFFFF" variant="link">
+      //         Termo de uso
+      //       </Button>
 
-            <Button colorScheme="#FFFFFF" color="#FFFFFF" variant="link">
-              Politica de privacidade
-            </Button>
+      //       <Button colorScheme="#FFFFFF" color="#FFFFFF" variant="link">
+      //          Direitos autorais
+      //       </Button>
 
-          </Stack>
-          </Grid>
+      //       <Button colorScheme="#FFFFFF" color="#FFFFFF" variant="link">
+      //         Politica de privacidade
+      //       </Button>
+      //       </ButtonGroup>
+          
+      //     {/* </Grid> */}
 
-        </Flex>
-      </Flex>
+      //   </Flex>
+      // </Grid>  
+
+
+
+
+
+
+
+
+
+      <Grid bg="#4E4B4B" width="100%" height="100%">
+         <Flex margin="15px" marginTop="150px" marginLeft="60px">
+           <Image src="/images/LOGOTIPO VILLA PORTAL- FOOTER.svg" alt="Logo" />
+         </Flex>
+         {/* <Flex> */}
+        
+         <Flex marginLeft="60px" marginTop="50px" marginBottom="60px">
+           <HStack>
+
+           <Button bg="#F6E05E" height="38px" width="38px" borderRadius="8px">
+             <Image src="/images/facebook.svg" alt="Logo" />
+           </Button>  
+
+           <Button bg="#F6E05E" height="38px" width="38px" borderRadius="8px">
+             <Image src="/images/instagram.svg" alt="Logo" />
+           </Button>   
+
+           <Button bg="#F6E05E" height="38px" width="38px" borderRadius="8px">
+             <Image src="/images/youtube.svg" alt="Logo" />
+           </Button> 
+         
+           </HStack> 
+           </Flex>
+
+
+           
+             {/* <Flex bg="pink"> */}
+               <ButtonGroup spacing="6" right="100px" position="absolute" marginTop="170px">
+                 <Button colorScheme="#FFFFFF" color="#FFFFFF" variant="link">
+                   Termo de uso
+                 </Button>
+
+                 <Button colorScheme="#FFFFFF" color="#FFFFFF" variant="link">
+                    Direitos autorais
+                 </Button>
+
+                 <Button colorScheme="#FFFFFF" color="#FFFFFF" variant="link">
+                   Politica de privacidade
+                 </Button>
+                 </ButtonGroup>
+             {/* </Flex> */}
+      {/* </Flex> */}
+      </Grid>
+
+
+
     );
 }
