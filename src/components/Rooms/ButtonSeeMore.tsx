1f71434acf764c84930d42f661b0f7ae55c67a84
rodlferreira <rodriggo.lferreira@icloud.com>
2021-05-18 12:18:53 -0300

    criaçao do link do botao <ver mais>

--- src/components/Rooms/ButtonSeeMore.tsx
+++ src/components/Rooms/ButtonSeeMore.tsx
@@ -1,10 +1,14 @@
 import { Button, Flex, ButtonGroup } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react"
+import { Link as ReachLink } from "@reach/router"
 
 export function ButtonSeeMore() {
     
     return(
         <Flex>
+            <Link as={ReachLink} to="/src/pages/seeMore/">
             <Button bg="#FEC200" color="#FFFFFF" borderRadius="15px">Ver mais...</Button>
+            </Link>
         </Flex>
     );
 }
